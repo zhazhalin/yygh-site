4b9aaf8fa494851c8b58c703a70d1feaf13c10a5
zhazhalin <1540741344@qq.com>
2022-02-23 00:44:32 +0800

    尚医通项目前台前端

---
+++ layouts/myfooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div className="footer-container">
+    <div className="wrapper">
+      <div><span className="record">京ICP备13018369号</span><span
+        className="phone">电话挂号010-56253825</span></div>
+      <div className="right"><span
+        className="v-link clickable"> 联系我们 </span><span
+        className="v-link clickable"> 合作伙伴 </span><span
+        className="v-link clickable"> 用户协议 </span><span
+        className="v-link clickable"> 隐私协议 </span></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {}
+</script>
